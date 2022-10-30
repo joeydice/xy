5415f5e3c6e9cba14a566474457d4aa9a7d1d565
joeydice <117003062+joeydice@users.noreply.github.com>
2022-10-30 01:42:46 -0700

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+UUID="e6e4cb70-4c07-43a3-a7ad-8382c5deb9ee"
+PORT=80
+# config xray
+cat << EOF > /etc/config.json
+{
+  "inbounds":[
+    {
+      "port": $PORT,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    },
+    {
+      "port": $PORT,
+      "protocol": "vless",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    },
+    {
+      "port": $PORT,
+      "protocol": "trojan",
+      "settings": {
+        "clients": [
+          {
+            "password": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}	
+EOF
+
+# run xray
+/usr/bin/xray run -config /etc/config.json
